36a2baa3e0a56b172c3482db45c11750452abbfc
Danny Thai <dthai92@yahoo.com>
2016-05-09 11:54:20 -0700

    [cleanup] reorganize directive into an IIFE

--- client/app/directives/linear-chart.directive.js
+++ client/app/directives/linear-chart.directive.js
@@ -1,98 +1,87 @@
+(function() {
+    angular
+      .module('baby.visual')
+      .directive('linearChart', linearChart);
 
-  angular
-    .module('baby.visual')
-    .directive('linearChart', linearChart);
+      function linearChart($window) {
+        return {
+          restrict: 'EA',
+          template: "<svg width='700' height='200'></svg>",
+          scope: {},
+          controller: 'VisualCtrl',
+          controllerAs: 'vm',
+          bindToController: {
+            salesDataToPlot: '='
+          },
+          link: function(scope, elem, attrs, controller) {
+            var salesDataToPlot = controller.salesData;;
+            var padding = 20;
+            var pathClass="path";
+            var xScale, yScale, xAxisGen, yAxisGen, lineFun;
+            var d3 = $window.d3;
+            var rawSvg=elem.find('svg');
+            var svg = d3.select(rawSvg[0]);
 
-    function linearChart($window) {
-      return {
-        restrict: 'EA',
-        template: "<svg width='700' height='200'></svg>",
-        scope: {},
-        controller: 'VisualCtrl',
-        controllerAs: 'vm',
-        bindToController: {
-          salesDataToPlot: '='
-        },
-        link: function(scope, elem, attrs, controller){
-          console.log('Link Running!');
-          console.log('this is scope', scope);
-          console.log('this is controller', controller);
-           // var salesDataToPlot=scope[attrs.chartData];
-           var salesDataToPlot = controller.salesData;;
-           var padding = 20;
-           var pathClass="path";
-           var xScale, yScale, xAxisGen, yAxisGen, lineFun;
+            function setChartParameters(){
+              xScale = d3.scale.linear()
+                .domain([salesDataToPlot[0].hour, salesDataToPlot[salesDataToPlot.length-1].hour])
+                .range([padding + 5, rawSvg.attr("width") - padding]);
 
-           console.log($window);
-           var d3 = $window.d3;
-           var rawSvg=elem.find('svg');
-           var svg = d3.select(rawSvg[0]);
+              yScale = d3.scale.linear()
+                .domain([0, d3.max(salesDataToPlot, function (d) {
+                  return d.sales;
+                })])
+                .range([rawSvg.attr("height") - padding, 0]);
 
-           function setChartParameters(){
-                console.log('setChartParameters running!');
-                console.log(salesDataToPlot)
-               xScale = d3.scale.linear()
-                   .domain([salesDataToPlot[0].hour, salesDataToPlot[salesDataToPlot.length-1].hour])
-                   .range([padding + 5, rawSvg.attr("width") - padding]);
+              xAxisGen = d3.svg.axis()
+                .scale(xScale)
+                .orient("bottom")
+                .ticks(salesDataToPlot.length - 1);
 
-               yScale = d3.scale.linear()
-                   .domain([0, d3.max(salesDataToPlot, function (d) {
-                       return d.sales;
-                   })])
-                   .range([rawSvg.attr("height") - padding, 0]);
+              yAxisGen = d3.svg.axis()
+                .scale(yScale)
+                .orient("left")
+                .ticks(5);
 
-               xAxisGen = d3.svg.axis()
-                   .scale(xScale)
-                   .orient("bottom")
-                   .ticks(salesDataToPlot.length - 1);
+              lineFun = d3.svg.line()
+                .x(function (d) {
+                    return xScale(d.hour);
+                  })
+                .y(function (d) {
+                    return yScale(d.sales);
+                  })
+                .interpolate("basis");
+            }
 
-               yAxisGen = d3.svg.axis()
-                   .scale(yScale)
-                   .orient("left")
-                   .ticks(5);
+            function drawLineChart() {
+              setChartParameters();
 
-               lineFun = d3.svg.line()
-                   .x(function (d) {
-                       return xScale(d.hour);
-                   })
-                   .y(function (d) {
-                       return yScale(d.sales);
-                   })
-                   .interpolate("basis");
-           }
+              svg.append("svg:g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0,180)")
+                .call(xAxisGen);
 
-         function drawLineChart() {
-              console.log('drawLineChart running!');
+              svg.append("svg:g")
+                .attr("class", "y axis")
+                .attr("transform", "translate(20,0)")
+                .call(yAxisGen);
 
-               setChartParameters();
+              svg.append("svg:path")
+                .attr({
+                  d: lineFun(salesDataToPlot),
+                  "stroke": "blue",
+                  "stroke-width": 2,
+                  "fill": "none",
+                  "class": pathClass
+                });
+            }
 
-               svg.append("svg:g")
-                   .attr("class", "x axis")
-                   .attr("transform", "translate(0,180)")
-                   .call(xAxisGen);
-
-               svg.append("svg:g")
-                   .attr("class", "y axis")
-                   .attr("transform", "translate(20,0)")
-                   .call(yAxisGen);
-
-               svg.append("svg:path")
-                   .attr({
-                       d: lineFun(salesDataToPlot),
-                       "stroke": "blue",
-                       "stroke-width": 2,
-                       "fill": "none",
-                       "class": pathClass
-                   });
+            drawLineChart();
+          }
         }
-
-           drawLineChart();
-       }
       }
 
+})()
 
-    }
-
-linearChart();
 
 
