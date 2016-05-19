642ffbf9a4a9ef695ff5aaee17d67933590ba637
Danny Thai <dthai92@yahoo.com>
2016-05-19 12:38:59 -0700

    update test spec pathway

--- protractor_conf.js
+++ protractor_conf.js
@@ -12,10 +12,14 @@ exports.config = {
 
   // Spec patterns are relative to the current working directory when
   // protractor is called.
-  specs: ['test/e2e/*_spec.js'],
+  specs: ['test/e2e/**/*.spec.js'],
 
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
-  }
+  },
+
+  baseUrl: 'http://127.0.0.1:8080',
+
+  seleniumAddress: 'http://127.0.0.1:4444/wd/hub'
 };
